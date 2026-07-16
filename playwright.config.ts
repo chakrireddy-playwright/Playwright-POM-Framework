7ce7935ea7262e0cdd177d41293bd9970d7e74fa
Chakrapani Reddy <chakrireddy9666@gmail.com>
2026-07-16 09:17:42 +0530

    Integrated Jira auto bug creation and secured environment configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     testDir: './tests',
 
     /* ✅ Run tests in parallel safely */
-    fullyParallel: true,
+    fullyParallel: false,
 
     /* ❌ Don’t stop full suite on first failure */
     forbidOnly: !!process.env.CI,
@@ -48,15 +48,16 @@ export default defineConfig({
         {
             name: 'chromium',
             use: { ...devices['Desktop Chrome'] },
-        },
-        {
-            name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
-        },
-        {
-            name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
         }
+            // ,
+        // {
+        //     name: 'firefox',
+        //     use: { ...devices['Desktop Firefox'] },
+        // },
+        // {
+        //     name: 'webkit',
+        //     use: { ...devices['Desktop Safari'] },
+        // }
     ],
 
     /* 📁 Output folders */
